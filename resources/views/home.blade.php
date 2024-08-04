4354294cad27b6aa558340874fffbeb7cbf578df
sepikode <sepikode@gmail.com>
2024-08-05 00:18:53 +0700

    test script module

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card zoomable">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
@@ -21,3 +21,26 @@
     </div>
 </div>
 @endsection
+@push('scripts')
+<script type="module">
+    $(document).ready(function(){
+     $(".zoomable").click(function(){
+         var currentFontSize = parseInt($(this).css("font-size"));
+         var targetFontSize;
+
+         if (currentFontSize < 40) {
+             targetFontSize = 40; // Zoom in
+             $(this).css('background-color', '#ff0000'); // Turn clicked paragraph text red using jQuery
+         } else {
+             targetFontSize = 16; // Zoom out
+             $(this).css('background-color', ''); // Change background color to white when zooming out
+         }
+
+         $(this).animate({
+             fontSize: targetFontSize + "px"
+         }, 1000);
+     });
+ });
+
+     </script>
+@endpush
